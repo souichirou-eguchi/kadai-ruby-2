02bf8709cf7a3c1cd1e43e7a4c01c0fdd2dcb3c8
souichirou.eguchi <soichiro.eguchi@fujiya-sc.co.jp>
2023-01-11 08:25:20 +0000

    kadai-2

---
+++ kadai-fizzbuzz.rb
@@ -0,0 +1,41 @@
+def FizzBazz()
+  num_max = 100
+  intFizz = 3
+  intBuzz = 5
+  
+  checkNum = 1
+  num_max.times do
+    case true
+    when checkNum % intFizz == 0 && checkNum % intBuzz == 0 then
+      returnString = 'FizzBazz'
+    when checkNum % intFizz == 0 then
+      returnString = 'Fizz'
+    when checkNum % intBuzz == 0 then
+      returnString = 'Buzz'
+    else
+      returnString = checkNum.to_s
+    end
+    puts returnString
+    checkNum += 1
+  end
+end
+
+def fizzbuzz(checkNum)
+  intFizz = 3
+  intBuzz = 5
+  
+  case true
+  when checkNum % intFizz == 0 && checkNum % intBuzz == 0 then
+    returnString = 'FizzBazz'
+  when checkNum % intFizz == 0 then
+    returnString = 'Fizz'
+  when checkNum % intBuzz == 0 then
+    returnString = 'Buzz'
+  else
+    returnString = checkNum.to_s
+  end
+  
+  return returnString
+end
+
+FizzBazz()
